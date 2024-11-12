8cc0dc12df33935201f794a23b92af82bbceb8f7
Larry Buza <lawrence.buza@coldquanta.com>
2024-11-11 21:21:48 -0600

    lualine

--- nvim/lua/custom/plugins/rust.lua
+++ nvim/lua/custom/plugins/rust.lua
@@ -1,11 +1,4 @@
 return {
-  recommended = function()
-    return LazyVim.extras.wants {
-      ft = 'rust',
-      root = { 'Cargo.toml', 'rust-project.json' },
-    }
-  end,
-
   -- LSP for Cargo.toml
   {
     'Saecki/crates.nvim',
@@ -78,9 +71,6 @@ return {
     },
     config = function(_, opts)
       vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
-      if vim.fn.executable 'rust-analyzer' == 0 then
-        LazyVim.error('**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/', { title = 'rustaceanvim' })
-      end
     end,
   },
 
